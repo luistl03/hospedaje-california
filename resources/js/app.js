3673b6ebf862dcb8120429febfe9707e763f8233
luistl03 <tantaleanletonal@gmail.com>
2026-05-26 07:56:39 -0500

    Sprint 1: Fundaciones, entorno local y stack tecnológico instalado

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
